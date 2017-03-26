791f0f14692d17577b49f563dcc96c313188abd4
aliakbar09a <aliakbar09a@gmail.com>
2017-03-26 18:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-  res.sendFile(create template(articleone));
+  res.sendFile(create template(articleOne));
 });
 
 app.get('/article-two',function(req,res){
