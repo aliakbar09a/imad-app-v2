63536fa4ebfbb1b81450cdeb3e443231bb9193c4
aliakbar09a <aliakbar09a@gmail.com>
2017-03-26 17:50:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var content={
+    title:'Article|One',
+    heading:'Article One',
+    date:'25 Mar 2017',
+    content:`<p>
+                    This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.
+                </p>
+                <p>
+                    This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.
+                </p>
+                <p>
+                    This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.This is my fist content.
+                </p>
+}
+function createtemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var content=data.content;
 
+    var htmltemplate=`
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>  
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+      +      <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    
+    
+    `;
+}
 var app = express();
 app.use(morgan('combined'));
 
