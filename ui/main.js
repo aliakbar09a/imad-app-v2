ba0613cf39c2e52ec00669c1c30459c00dcf9287
aliakbar09a <aliakbar09a@gmail.com>
2017-06-15 03:25:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 console.log('Loaded!');
 
-var img =document.getElementById('madi');
+var element = document.getElementById('main-text');
+element.innerHTML= 'NEW VALUE';
+var img = document.getElementById('madi');
 
 img.onclick = function() {
     img.style.marginleft='100px';
