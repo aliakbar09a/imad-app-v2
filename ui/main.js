485821e421d31b95f2e966e7949673d02ed524ef
aliakbar09a <aliakbar09a@gmail.com>
2017-06-22 01:24:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,15 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft=marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
-};
+var button = document.getElementById('counter');
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if (request.readyState==XMLHttpRequest.DONE)
+        {
+            if (request.status == 200){
+                var counter =request.responseText;
+                var span = document.getElementById('count');
+                span.innerHtml = counter.toString();
+            }
+        }
+    }
+} 
